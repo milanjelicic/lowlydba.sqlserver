251f6ea5d13bd528e3e8b3c21d8d52a902f07a1f
Milan Jelicic <mjelicic@nil.com>
2024-02-28 21:01:32 +0100

    Aff get_replicas function

---
+++ plugins/modules/get_replicas.ps1
@@ -0,0 +1,37 @@
+#!powershell
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#AnsibleRequires -PowerShell ansible_collections.lowlydba.sqlserver.plugins.module_utils._SqlServerUtils
+#Requires -Modules @{ ModuleName="dbatools"; ModuleVersion="2.0.0" }
+
+$ErrorActionPreference = "Stop"
+
+# Get Csharp utility module
+$spec = @{
+    supports_check_mode = $true
+    options = @{
+        ag_name = @{type = 'str'; required = $true }
+    }
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec, @(Get-LowlyDbaSqlServerAuthSpec))
+$sqlInstance, $sqlCredential = Get-SqlCredential -Module $module
+$agName = $module.Params.ag_name
+$module.Result.changed = $false
+
+try {
+    $availabilityGroup = Get-DbaAvailabilityGroup -SqlInstance $sqlInstance -AvailabilityGroup $agName -SqlCredential $sqlCredential
+
+    if ($null -ne $availabilityGroup) {
+        $resultData = $availabilityGroup.AvailabilityReplicas | ForEach-Object { $_.Name }
+        $module.Result.data = $resultData
+    }
+    $module.ExitJson()
+}
+catch {
+    $module.FailJson("Error getting ${ag_name} replicas for ${sqlInstance} instance.", $_)
+}
---
+++ plugins/modules/get_replicas.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = r'''
+---
+module: get_replicas
+short_description: Get AG replicas
+description:
+  - Get information about availability group replicas in a SQL Server Always On AG.
+version_added: 2.3.0
+options:
+  sql_instance:
+    description:
+      - The SQL Server instance to lookup.
+    type: str
+    required: true
+  ag_name:
+    description:
+      - Name of the Availability Group to lookup.
+    type: str
+    required: true
+  username:
+    description:
+      - Username for alternative credential to authenticate with Windows.
+    type: str
+    required: false
+  password:
+    description:
+      - Password for alternative credential to authenticate with Windows.
+    type: str
+    required: false
+author: "Milan Jelicic (@milanjelicic)"
+requirements:
+  - L(dbatools,https://www.powershellgallery.com/packages/dbatools/) PowerShell module
+extends_documentation_fragment:
+  - lowlydba.sqlserver.sql_credentials
+  - lowlydba.sqlserver.attributes.check_mode
+  - lowlydba.sqlserver.attributes.platform_win
+'''
+
+EXAMPLES = r'''
+- name: Get AG replicas
+  lowlydba.sqlserver.get_replicas:
+    sql_instance: 'sql-01.myco.io'
+    sql_username: 'sa'
+    sql_password: 'password'
+    ag_name: 'AG01'
+'''
+
+RETURN = r'''
+data:
+  description:
+    - Availability Replicas from the C(Get-DbaAvailabilityGroup) function.
+  returned: success, but not in check_mode.
+  type: list
+'''
